bdb74c51fe55538aaf343ac98af17e20f9ff76e5
Caio Centeio <cfcenteio@gmail.com>
2023-08-24 16:08:06 -0300

    actualy working

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :movies, only: %i[index show]
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[new create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
+
+# <div class="row justify-content-center">
+# <div class="col-4">
+#   <h1>Add a bookmark</h1>
+
+#   <%= simple_form_for [@list, @bookmark] do |f| %>
+#     <%= f.input :comment %>
+#     <%= f.association :movie %>
+#     <%= f.submit %>
+#   <% end %>
+# </div>
+# </div>
